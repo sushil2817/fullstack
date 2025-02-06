f57d01cc56ac7a7bbc9eaa145fd96b4c84dce4c3
sushil2817 <tony281723@gmail.com>
2025-02-06 08:49:40 +0530

    learn about some basic of  functions

--- JavaScript/hello.js
+++ JavaScript/hello.js
@@ -5,5 +5,15 @@ function printHello(){
     console.log("Hello world! this is SUSHIL");
 }
 
-printHello();
+function bringBrush(itne){
+    console.log(`Hanji, le aaya ${itne} brush`)
+}
 
+function addTwo(ekNum,doNum){
+    return ekNum+doNum
+}
+
+bringBrush(5);
+printHello();
+addTwo(1,1)
+console.log(addTwo(2,2))
