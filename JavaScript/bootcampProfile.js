02bbdc78fefb8a5f44c38d44afaf1240609f900f
sushil2817 <tony281723@gmail.com>
2025-02-12 08:47:06 +0530

    add initial JavaScript student profile with basic data types and console logs

---
+++ JavaScript/bootcampProfile.js
@@ -0,0 +1,31 @@
+// string -> sequence of characters-> "Sushil", 'Sushil',`Sushil`
+let name = "Sushil"
+// Number - 25, 25.29
+let age = 25
+// Boolean -> true/false
+let isPaid = true
+
+let favouriteClass = null // nothing
+
+let hometown; // not defined -> bad me dekhehge
+
+let skills = ["HTML", "CSS", "JavaScript", "React", "NodeJS"]
+
+let studentProfile = {
+    name: "Sushil",
+    age: 25,
+    isPaid: true,
+    favouriteClass: null,
+    hometown: undefined,
+    skills: ["HTML", "CSS", "JavaScript", "React", "NodeJS"]
+}
+
+console.log(name);
+console.log(age);
+console.log(isPaid);
+console.log(favouriteClass);
+console.log(hometown);
+console.log(skills);
+console.log(studentProfile);
+
+
