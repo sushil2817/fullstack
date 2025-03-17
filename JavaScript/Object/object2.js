42b4438dfc2fff367aec70a2377f7ee2114a890a
sushil2817 <tony281723@gmail.com>
2025-03-17 22:26:20 +0530

    Mastering Objects and understand about serialazation and deserialzation

--- JavaScript/Object/object2.js
+++ JavaScript/Object/object2.js
@@ -1,10 +1,36 @@
 let s1 = {
     fname:'Sushil',
+    lname:"Kumawat",
+    address:{
+        h:1,
+        s:1,
+        w:3
+    }
 } 
 
-let s2 = s1
+const s1kaString  = JSON.stringify(s1)
 
-s2.fname = 'Sushil Kumawat'
+let s2 = JSON.parse(s1kaString)
 
-console.log(s2);
+
+
+// let s2 = {
+//     fname:s1.fname,
+//     lname:s1.lname,
+//     address: s1.address
+// }
+
+// let s2 = {
+    // ...s1, // Spread operator ...  Done work on inner object  make shallow object
+    // address:{
+    //     ...s1.address
+    // }
+//}
+
+
+
+s2.fname = 'Sushil2'
+s2.address.h = 2
+
 console.log(s1);
+console.log(s2);
