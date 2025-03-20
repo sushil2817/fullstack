73bffc2ed40bda657a7417f1a7b07373d4981a8a
sushil2817 <tony281723@gmail.com>
2025-03-20 22:30:31 +0530

    learned about pollyfills

--- JavaScript/Object/MachineCodingRound/prototype.js
+++ JavaScript/Object/MachineCodingRound/prototype.js
@@ -1,10 +1,56 @@
-const arr = [1,2,3,4,5];
+// const arr = [1,2,3,4,5];
 
-arr.prototype.sushil = function(){
-    console.log("Sushil");
+// arr.prototype.sushil = function(){
+//     console.log("Sushil");
+// }
+// const arr2 = [1,2,4,4,5]
+// // console.log(arr2.sushil)
+
+// if(!Array.prototype.fill){
+//     // fallback
+//     Array.prototype.fill = function(){
+//         console.log("Sushil");
+//     }
+// }
+
+
+// arr.fill()
+
+
+const arr = [1,2,3,4,5,6];
+// Error: .forEach function does not exist on arr variable 
+
+// Real Signature ko samjo- No return, funtion input, value, index
+// call my fn for every value
+
+if(!Array.prototype.myForEach){
+    Array.prototype.myForEach = function(userFn){
+        const originalArr = this
+        for(let i=0;i<originalArr.length;i++){
+            userFn(originalArr[i],i)
+        }
+    }
 }
 
+const rat = arr.myForEach(function(value,index){
+    console.log(`My For Each Value at Index${index} is ${value}`);
+    
+})
 
-const arr2 = [1,2,4,4,5]
+console.log(rat);
 
-console.log(arr2.sushil)
+
+// Signature- .map
+// Return? - New Array, Each ele Iterate,userFn
+
+if(!Array.prototype.myMap){
+    Array.prototype.MyMap = function(useFn){
+        const result = []
+
+        for(let i=0;i<this.length;i++){
+            const value = useFn(this[i],i)
+            result.push(value)
+        }
+        return result
+    }
+}
---
+++ JavaScript/Object/MachineCodingRound/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+ray.prototype.myForEach){
+//     Array.prototype.myForEach = function(userFn){
+//         const originalArr = this
+//         for(let i=0;i<originalArr.length;i++){
+//             userFn(originalArr[i],i)
+//         }
+//     }
+// }
