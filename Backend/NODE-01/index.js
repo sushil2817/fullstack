fc4978de4ef3a00e9be0b01553728ccfb9a35817
sushil2817 <tony281723@gmail.com>
2025-03-24 09:00:46 +0530

    deep learning about node

--- Backend/NODE-01/index.js
+++ Backend/NODE-01/index.js
@@ -14,9 +14,17 @@ console.log(math.subtract(5,7));
 function __require(id){
     // ... agar id ./ se shuru hua toh user ki dir me code ko dhundo
     // .. Khud ke internal module mei dhundo
+    // .. agar toh mill gya toh well and good
+    // .. nahi to node_modules mei dhundo
+    // .. user ko error throe karo
 }
 
 
 // executeWrapperDxn(exports, require,module, __dirname,__filename, ){
 
 // }
+
+
+// require('fs') = Node built in
+// require('./math') = Current dir mei math name ki file ko  
+// require('../math') = Ek dir bhar jao aur parent mei math file ko dekho
