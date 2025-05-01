6b1ba9ed7149a233fceb35c1b1133f428d9428ee
sushil2817 <tony281723@gmail.com>
2025-05-01 13:24:40 +0530

    backend cors and middleware

--- FullStack-Project/index.js
+++ FullStack-Project/index.js
@@ -1,14 +1,18 @@
 import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config();
 
 const app = express()
-const port = 3000 // total port 65535
+const port = process.env.PORT || 3000 // total port 65535
 
 app.get('/', (req, res) => {
   res.send('Sushil Cohort!')
 })
 
 app.get('/sushil', (req, res) => {
-  res.send('Nisha!');
+  res.send('sushil!');
 })
 
 app.listen(port, () => {
