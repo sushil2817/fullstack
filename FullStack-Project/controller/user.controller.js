108cc742919d0d4f0288e4afb4f78eec49a8683c
sushil2817 <tony281723@gmail.com>
2025-05-09 22:06:10 +0530

    write some controllers

--- FullStack-Project/controller/user.controller.js
+++ FullStack-Project/controller/user.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/User.model"
+
 const registerUser = async (req,res) =>{
     // create a new user
         // get data
@@ -9,15 +11,32 @@ const registerUser = async (req,res) =>{
         //send token as email to user
         //send success status to user
 
-        console.log(req)
+        // console.log(req)
+
+        
 
         const {name,email,password} = req.body
 
+        // if(password.length > 6){
+        //     console.log("Passwor dis less then 6")
+        //    }
         if(!name || !email || !password){
             return res.status(400).json({
                 messgae:"All fileds are required"
             })
         }
+        
+        try{
+            const exisitingUser = await User.findOne({email})
+            if(exisitingUser){
+                return res.status(400).json({
+                    message:"User already exists"
+                })
+            }
+        }catch (error){
+
+        }
+        
 
 };
 
