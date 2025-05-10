d9c8d6ec76097e97a1361fcf5575cc7d1acc3dfb
sushil2817 <tony281723@gmail.com>
2025-05-10 23:28:08 +0530

    next step in backend

--- FullStack-Project/routes/user.routes.js
+++ FullStack-Project/routes/user.routes.js
@@ -1,9 +1,5 @@
 import express from "express"
 import { registerUser } from "../controller/user.controller.js"
-
-
 const router = express.Router()
-
 router.post("/register", registerUser)
-
 export default router
