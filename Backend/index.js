3fa1c969046373b2f83cc36f143d25f4141eb2bb
sushil2817 <tony281723@gmail.com>
2025-05-25 21:03:09 +0530

    learn aboput data types in python

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,15 @@
 const fs =  require('fs')
 
+
 setTimeout(()=>console.log('Set Timeout'), 0)
 setImmediate(()=> console.log('Set Immediate') );
+
 
 fs.readFile('sample.txt','utf-8',function(error,data){
     
 setTimeout(()=>console.log('Set Timeout inside fs 1'), 0)
 setImmediate(()=> console.log('Set Immediate inside fs 1') );
 })
+
 
 console.log('hello');
