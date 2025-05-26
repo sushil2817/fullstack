ada23af829998d583471c116d1cbd419ede3c7fe
sushil2817 <tony281723@gmail.com>
2025-05-26 23:22:49 +0530

    create some server and using node and express

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,9 @@ setImmediate(()=> console.log('Set Immediate') );
 fs.readFile('sample.txt','utf-8',function(error,data){
     
 setTimeout(()=>console.log('Set Timeout inside fs 1'), 0)
+
 setImmediate(()=> console.log('Set Immediate inside fs 1') );
+
 })
 
 
