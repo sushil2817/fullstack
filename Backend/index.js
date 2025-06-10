5fc6d1f8e26e3223c39348b60d4ce81ba744f991
sushil2817 <tony281723@gmail.com>
2025-06-10 08:51:06 +0530

    learn about node internal

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,27 @@
-const fs =  require('fs')
+// const fs =  require('fs')
 
 
-setTimeout(()=>console.log('Set Timeout'), 0)
-setImmediate(()=> console.log('Set Immediate') );
+// setTimeout(()=>console.log('Set Timeout'), 0)
+// setImmediate(()=> console.log('Set Immediate') );
 
 
-fs.readFile('sample.txt','utf-8',function(error,data){
+// fs.readFile('sample.txt','utf-8',function(error,data){
     
-setTimeout(()=>console.log('Set Timeout inside fs 1'), 0)
+// setTimeout(()=>console.log('Set Timeout inside fs 1'), 0)
 
-setImmediate(()=> console.log('Set Immediate inside fs 1') );
+// setImmediate(()=> console.log('Set Immediate inside fs 1') );
 
-})
+// })
 
 
-console.log('hello');
+// console.log('hello');
+
+
+
+const fs = require('fs'); //1
+
+setTimeout(()=> console.log('Set Timeout'), 0); //2
+setImmediate(()=> console.log('Set Immediate'));   //3 
+
+console.log('Hello'); //4-> 1(Top level code)
+
