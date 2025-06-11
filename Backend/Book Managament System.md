85ba9b89466f6207243282e8aac9de0acd9c63b6
sushil2817 <tony281723@gmail.com>
2025-06-11 22:34:18 +0530

    create some db models

---
+++ Backend/Book Managament System.md
@@ -0,0 +1,59 @@
+# Book Managment System
+
+### Table
+- Users
+    - Id
+    - Name
+    - Phone Number
+    - Address
+    - Role: 'ADMIN' or 'USER'
+- Books
+    - ID
+    - Name
+    - ISBN_NUMBER
+    - Price
+    - Author
+- Author
+  - ID
+  - Name
+  - Instagram
+  - TikTok
+  - List[Books]
+- BookRecords
+  - ID
+  - Book ID
+  - User ID
+  - Issue Time
+  - Retrun Time
+  - Is Returned
+
+
+# Techyst (Next Gen LMS)
+Heads Up: Techyst is white labelled
+> Connect your own domain
+> Connect your own Payment Provider
+> Connect your own Integrations
+> Connect 
+
+## Table
+- User
+- Courses
+- Lessons
+  - ID
+  - Type
+- Enrollments
+- Transactions
+  - ID
+  - User ID
+  - Course ID
+  - Payment Gateway
+  - Amount
+  - Date
+- Coupon
+  - Code
+  - Discount Type
+  - Value 
+  - Start Time 
+- Category
+
+# Youtube
--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@
 
 
 // fs.readFile('sample.txt','utf-8',function(error,data){
-    
+
 // setTimeout(()=>console.log('Set Timeout inside fs 1'), 0)
 
 // setImmediate(()=> console.log('Set Immediate inside fs 1') );
@@ -17,11 +17,33 @@
 // console.log('hello');
 
 
-
 const fs = require('fs'); //1
+const crypto = require('crypto');
+setTimeout(() => console.log('Set Timeout'), 0); //2
+setImmediate(() => console.log('Set Immediate'));   //3 
 
-setTimeout(()=> console.log('Set Timeout'), 0); //2
-setImmediate(()=> console.log('Set Immediate'));   //3 
+fs.readFile('sample.txt', 'utf-8', function (error, data) { //4
+    setTimeout(() => console.log('Set Timeout inside fs'), 0); //5
+    setImmediate(() => console.log('Set Immediate inside fs')); //6
 
-console.log('Hello'); //4-> 1(Top level code)
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => { //7
+        console.log('Hashing done'); //8
+    });
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => { //7
+        console.log('Hashing done'); //8
+    });
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => { //7
+        console.log('Hashing done'); //8
+    });
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => { //7
+        console.log('Hashing done'); //8
+    });
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => { //7
+        console.log('Hashing done'); //8
+    });
 
+
+    console.log('Hello'); //4-> 1(Top level code)
+
+
+});
