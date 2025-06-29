018059a604fb235b13f7be4d9a24564fabfa3950
sushil2817 <tony281723@gmail.com>
2025-06-29 22:00:13 +0530

    learn about class in js

--- JavaScript/Object & Class/Class1.js
+++ JavaScript/Object & Class/Class1.js
@@ -1,20 +1,20 @@
 // Prototype Inheritance // Chaining
 
 class Person {
-    constructor(fname,lname){
+    constructor(fname, lname) {
         this.fname = fname;
         this.lname = lname;
     }
 
-    getFullName(){
+    getFullName() {
         return `${this.fname} ${this.lname}`
     }
 }
 
 // syntext sugar
 
-const p1 = new Person('Sushil','Kumawat')
-const p2 = new Person('done','Kumawat') 
+const p1 = new Person('Sushil', 'Kumawat')
+const p2 = new Person('done', 'Kumawat')
 // console.log(p1.getFullName())
 // console.log(p2.getFullName())
 
@@ -24,16 +24,16 @@ const p2 = new Person('done','Kumawat')
 
 
 const obj1 = {
-    fname:"Sushil",
-    lname:"Kumawat",
-    getFullName:function(){
-        if(this.lname !== undefined) return `${this.fname} ${this.lname}`;
+    fname: "Sushil",
+    lname: "Kumawat",
+    getFullName: function () {
+        if (this.lname !== undefined) return `${this.fname} ${this.lname}`;
         return `${this.fname} ${this.lname}`;
     }
 };
 const obj2 = {
-    fname:"done",
-    lname:"Kumawat",
+    fname: "done",
+    lname: "Kumawat",
 }
 obj2.__proto__ = obj1
 // obj1.__proto__= null
@@ -42,16 +42,16 @@ console.log(obj1.getFullName());
 console.log(obj2.getFullName());
 console.log(obj2.toString());
 // Arrow function
-const add = (x,y)=>{
-    return x+y
+const add = (x, y) => {
+    return x + y
 }
 
-const addSum = (x,y) => returnx+y
+const addSum = (x, y) => return x + y
 
-const addSum2 = (x,y)=> x+y
+const addSum2 = (x, y) => x + y
 
-arr.map((value)=> value*2);
+arr.map((value) => value * 2);
 
-arr.map(function(value){
-    return value*2
+arr.map(function (value) {
+    return value * 2
 })
