083c164b8171ab8fe0818fe0617d5f0c86a35dca
sushil2817 <tony281723@gmail.com>
2025-07-08 23:11:39 +0530

    learn abour DOM

--- JavaScript/DOM/16Feb.js
+++ JavaScript/DOM/16Feb.js
@@ -6,23 +6,17 @@ function greet(name){
 }
 greet.describe(); //
 console.log(greet('sushil'))
-
 function add(a,b){
     return a+b
 }
-
 const substract = function(a,b){
     return a-b
 }
-
 const multiply = (a,b) => a*b
-
 function applyOperation(a,b, operation){
     return operation(a,b)
 }
-
 const result = applyOperation(5,4,(x,y) => x/y)
-
 function createCounter(){
     let count = 0;
     return function(){
@@ -30,9 +24,7 @@ function createCounter(){
         return count
     }
 }
-
 function onef(){
     let myName = "Sushil"
 }
-
 console.log(myName );
