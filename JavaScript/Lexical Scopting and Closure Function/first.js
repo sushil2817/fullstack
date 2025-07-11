3b9002bb30485b22fd5c664d5ca5688b1e67432a
sushil2817 <tony281723@gmail.com>
2025-07-11 22:57:14 +0530

    learna about lexical scope

--- JavaScript/Lexical Scopting and Closure Function/first.js
+++ JavaScript/Lexical Scopting and Closure Function/first.js
@@ -1,27 +1,36 @@
-let fname = 'Sushil'
+// let fname = 'Sushil'
 
-// Cannot [redeclare] block-scoped variable 'fname'.
+// // Cannot [redeclare] block-scoped variable 'fname'.
+// function sayName(){
+//     // let fname = 'Nisha'
+//     let lname = 'Kumawat';
+//     function inner(){
+//         console.log('Fname is ',fname,' Lname is ',lname);  
+//         return  
+//     }
+//     inner()
+// }
+// console.log('Value of Name id  ',fname);
+// sayName()   
+// function increment(){
+//     let count = 0;
+//     return function(){
+//         return count++
+//     }
+// }
+// const x = increment()
+// const y = increment()
+
+let fname = 'Sushil'
+// let fname = 'Nisha'
+//Cannot redeclare block-scoped variable 'fname'.
 function sayName(){
-    // let fname = 'Nisha'
-    let lname = 'Kumawat';
+    let lname = 'SomeOne';
     function inner(){
-        console.log('Fname is ',fname,' Lname is ',lname);  
-        return  
+        console.log('Fname is', fname, 'lname is ',lname);
+        return;
     }
     inner()
 }
-
-
-console.log('Value of Name id  ',fname);
-sayName()   
-
-
-function increment(){
-    let count = 0;
-    return function(){
-        return count++
-    }
-}
-
-const x = increment()
-const y = increment()
+console.log('Value of Fname is ',fname);
+sayName()
