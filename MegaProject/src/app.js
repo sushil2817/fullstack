d08cc9774ce5a27dde5ef7aff903192a71c2feb5
sushil2817 <tony281723@gmail.com>
2025-08-22 08:37:49 +0530

    project setup

--- MegaProject/src/app.js
+++ MegaProject/src/app.js
@@ -2,6 +2,9 @@ import express from "express"
 
 const app = express();
 
+// router imports
+import { healthChekc } from "./controllers/healthcheck.controllers.js";
 
+app.use("/api/v1/healthcheck",healthChekc)
 
 export default app;
--- MegaProject/src/models/user.model.js
+++ MegaProject/src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import crypto from "crypto"
 
 const userSchema = new Schema({
     avatar:{
@@ -62,6 +64,43 @@ userSchema.pre("save",async function(next){
     next()
 })
 
-// 2:52
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken =  function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn:process.env.ACCESS_TOKEN_EXPIRY}
+    );
+};
+
+userSchema.methods.generateRefreshToken =  function(){
+    return jwt.sign({
+        _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {expiresIn:process.env.REFRESH_TOKEN_EXPIRY}
+    );
+};
+
+userSchema.methods.generateTemporaryToken = async function(){
+    const unHashedToken = crypto.randomBytes(30),toString("hex")
+
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(unHashedToken)
+        .digest("hex")
+    
+    const tokenExpiry = Date.now()+ (20*60*1000) //20min
+
+    return {hashedToken,unHashedToken, unHashedToken}
+}
+
+
 
 export const User = mongoose.model("User",userSchema);
