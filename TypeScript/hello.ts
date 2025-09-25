c0b007c3e69dcabb315a0baf9d817c02657cc12b
sushil2817 <tony281723@gmail.com>
2025-09-25 08:15:30 +0530

    refactor: improve createUser function by adding validation for lastname

--- TypeScript/hello.ts
+++ TypeScript/hello.ts
@@ -1,12 +1,15 @@
 function add(x:number, y: number){
     return x+y;
+
 }
 
 function createUser(user:{firstname:string; lastname?:string}){
     if(!user.lastname) return;
     const trimmedLastName:string = user.lastname.trim()
 }
+
 let x = add(4,4).toFixed(3)
+
 createUser({
     firstname:'Sushil',
     lastname:'Kumawat'
