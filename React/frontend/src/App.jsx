e3661ff752daad19a935c2fcbe6786f1f5f04300
sushil2817 <tony281723@gmail.com>
2025-10-07 22:43:44 +0530

    refactor: improve formatting in App.jsx by adding whitespace for better readability

--- React/frontend/src/App.jsx
+++ React/frontend/src/App.jsx
@@ -17,9 +17,13 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+
       <div className="card">
+        
         <button onClick={() => setCount((count) => count + 1)}>
+        
           count is {count}
+        
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
