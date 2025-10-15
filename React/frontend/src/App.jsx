882c1bbc99f5adf794b2256f0c6b95af0d2eb9c1
sushil2817 <tony281723@gmail.com>
2025-10-15 10:55:59 +0530

    refactor: add whitespace for improved readability in App.jsx

--- React/frontend/src/App.jsx
+++ React/frontend/src/App.jsx
@@ -15,12 +15,15 @@ function App() {
         <Login/>
         <Login/>
         <Login/>
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        
       </div>
       <h1>Vite + React</h1>
 
