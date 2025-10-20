805ad8a8e73225d3782368483bb1c856775ed123
sushil2817 <tony281723@gmail.com>
2025-10-20 17:36:58 +0530

    refactor: improve code formatting and readability in index.html and main.jsx

--- React/frontend/src/main.jsx
+++ React/frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter,Routes, Route} from "react-router"
+import { BrowserRouter, Routes, Route } from "react-router"
 import Login from './components/Login.jsx'
 import Signup from './components/Signup.jsx'
 import Otp from './components/Otp.jsx'
@@ -10,17 +10,14 @@ import Otp from './components/Otp.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <Routes>
-
-      <Route path="/" element={<App/>}></Route>
-      <Route path="/login" element={<Login/>}>
-
-        <Route path="otp" element={<Otp/>}/>
-      
-      </Route>
-      <Route path="/signup" element={<Signup/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/login" element={<Login />}>
+          <Route path="otp" element={<Otp />} />
+        </Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
     </BrowserRouter>
-    
+
   </StrictMode>,
 )
