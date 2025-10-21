0682644a805610aaa145c143567b4ecf970329fc
sushil2817 <tony281723@gmail.com>
2025-10-21 17:37:48 +0530

    refactor: add whitespace for improved readability in Otp.jsx

--- React/frontend/src/components/Otp.jsx
+++ React/frontend/src/components/Otp.jsx
@@ -2,7 +2,9 @@ function Otp(){
     return (
         <div>
             <h1>Welcome to Otp page</h1>
+
             <p>Lorem ipsum dolor sit amet consectetur </p>
+            
         </div>
     )
 
