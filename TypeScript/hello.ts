d621eed3e9b5bb056ea4cb7e9dcf78ea138cd5f2
sushil2817 <tony281723@gmail.com>
2025-10-31 16:33:06 +0530

    refactor: remove unnecessary whitespace for improved code formatting in hello.ts

--- TypeScript/hello.ts
+++ TypeScript/hello.ts
@@ -2,29 +2,23 @@ function add(x: number, y: number) {
     return x + y;
 
 }
-
 function createUser(user: { firstname: string; lastname?: string }) {
     if (!user.lastname) return;
     const trimmedLastName: string = user.lastname.trim()
 }
-
 interface User {
     firstname: string;
     lastname?: string;
     email: string;
     profileImageURL?: string;
 }
-
 function updateUser(user: User) {
     user
 }
-
 const payload: User = {
     firstname: '',
     email: ''
 }
-
-
 updateUser(payload)
 
 let x = add(4, 4).toFixed(3)
