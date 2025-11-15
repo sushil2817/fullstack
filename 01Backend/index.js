a412fbbf57ad31b18f90d05fc4b1b7a790fe2b07
sushil2817 <tony281723@gmail.com>
2025-11-15 20:09:11 +0530

    refactor: remove unnecessary whitespace for improved code formatting in index.js

--- 01Backend/index.js
+++ 01Backend/index.js
@@ -2,9 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import db from './utils/db.js'
-
 dotenv.config()
-
 const app = express()
 app.use(
     cors({
@@ -17,14 +15,11 @@ app.use(
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 const port = process.env.PORT || 8080
-
 app.get('/', (req, res) => {
     res.send("Sushil")
 })
-
 // DataBase Connection
 db();
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
