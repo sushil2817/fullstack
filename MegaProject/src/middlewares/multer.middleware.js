ca211635edb8d2ee3d902ae146426b7de462be16
sushil2817 <tony281723@gmail.com>
2025-11-28 22:27:41 +0530

    fix: improve formatting in multer.middleware.js

--- MegaProject/src/middlewares/multer.middleware.js
+++ MegaProject/src/middlewares/multer.middleware.js
@@ -1,18 +1,16 @@
 import multer from "multer"
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null,"./public/images")
+        cb(null, "./public/images")
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
         cb(null, file.fieldname + '-' + uniqueSuffix)
     }
 });
-
-export const upload = multer({ 
+export const upload = multer({
     storage: storage,
-    limits:{
-        fileSize: 1*1000*1000,
+    limits: {
+        fileSize: 1 * 1000 * 1000,
     },
 });
