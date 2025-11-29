29e2401df4ff2f0005c28f526270fbc98ebadd40
sushil2817 <tony281723@gmail.com>
2025-11-29 22:39:27 +0530

    fix: add missing blank lines for improved readability in multer.middleware.js

--- MegaProject/src/middlewares/multer.middleware.js
+++ MegaProject/src/middlewares/multer.middleware.js
@@ -1,4 +1,5 @@
 import multer from "multer"
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "./public/images")
@@ -8,6 +9,8 @@ const storage = multer.diskStorage({
         cb(null, file.fieldname + '-' + uniqueSuffix)
     }
 });
+
+
 export const upload = multer({
     storage: storage,
     limits: {
