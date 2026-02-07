f3d3e98232eb8cfe13408a939ecc833c02be6105
sushil2817 <tony281723@gmail.com>
2026-02-07 10:44:10 +0530

    Create rect app

---
+++ React0/src/App.js
@@ -0,0 +1,17 @@
+import React from "https://esm.sh/react@19.2.0";
+import ReactDOM from "https://esm.sh/react-dom@19.2.0/client";
+const App = () =>{
+    return React.createElement(
+        "div",
+        {class:"test"},
+        React.createElement(
+            "h1",
+            {},
+            "Chai, chill and react - React-18"
+        )
+    );
+};
+
+const container = document.getElementById("root")
+const root = ReactDOM.createRoot(container)
+root.render(React.createElement(App))
