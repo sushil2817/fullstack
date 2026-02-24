0b56c8fcf6e4bedf77f424bf9d65886749f5aac9
sushil2817 <tony281723@gmail.com>
2026-02-24 23:12:28 +0530

    Deep dive into react and learn about react boiler plate code

--- React0/src/App.js
+++ React0/src/App.js
@@ -1,14 +1,22 @@
 import React from "https://esm.sh/react@19.2.0";
 import ReactDOM from "https://esm.sh/react-dom@19.2.0/client";
+
+const Chai = (props) =>{
+    return React.createElement("div",{},[
+        React.createElement("h1",{},props.name),
+        React.createElement("h5",{},props.cost),
+    ]);
+};
+
 const App = () =>{
     return React.createElement(
         "div",
         {class:"test"},
-        React.createElement(
-            "h1",
-            {},
-            "Chai, chill and react - React-18"
-        )
+        React.createElement("h1",{},"Chai variations by chaicode",),
+        React.createElement(Chai,{name:"Masala Chai",cost:"600"}),
+        React.createElement(Chai,{name:"Ginger Chai",cost:"900"}),
+        React.createElement(Chai,{name:"Lemon Tea",cost:"800"}),
+        React.createElement(Chai,{name:"Ice Tea",cost:"700"}),
     );
 
 };
