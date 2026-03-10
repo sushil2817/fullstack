c3ac45f95259a0836ad42b1bdcbf190faf9585d4
sushil2817 <tony281723@gmail.com>
2026-03-10 23:11:43 +0530

    Learn about useEffect, useState hooks

--- React0/04React/src/App.jsx
+++ React0/04React/src/App.jsx
@@ -1,8 +1,18 @@
+import {useState,useEffect} from "react"
 export function App(){
+    const [message, setMessge] = useState("Loading...")
+    
+    useEffect(()=>{
+        fetch(`http://localhost:3000/api`)
+            .then((res)=>res.json())
+            .then((data)=>setMessge(data.message))
+            .catch((err)=>setMessge ("Failed to Load"))
+    },[])
     return (
         <div>
             <h1>Welcom to chai code</h1>
             <p>Serving hot chai with react</p>
+            <h2>{message}</h2>
         </div>
     )
 }
