02341e86240feebfd2c37223343f66af376425c3
sushil2817 <tony281723@gmail.com>
2026-03-11 21:26:04 +0530

    changes

--- React0/04React/src/App.jsx
+++ React0/04React/src/App.jsx
@@ -5,14 +5,20 @@ export function App(){
     useEffect(()=>{
         fetch(`http://localhost:3000/api`)
             .then((res)=>res.json())
+
             .then((data)=>setMessge(data.message))
+
             .catch((err)=>setMessge ("Failed to Load"))
     },[])
     return (
         <div>
+
             <h1>Welcom to chai code</h1>
+
             <p>Serving hot chai with react</p>
+
             <h2>{message}</h2>
+            
         </div>
     )
 }
