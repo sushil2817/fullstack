6f0d226756c5ad0bc356f3d79bf1efbe7f0959e4
sushil2817 <tony281723@gmail.com>
2026-03-18 14:09:36 +0530

    modify the code

--- React0/src/App.js
+++ React0/src/App.js
@@ -1,14 +1,11 @@
 import React from "https://esm.sh/react@19.2.0";
 import ReactDOM from "https://esm.sh/react-dom@19.2.0/client";
-
 const Chai = (props) =>{
     return React.createElement("div",{},[
         React.createElement("h1",{},props.name),
         React.createElement("h5",{},props.cost),
     ]);
 };
-
-
 const App = () =>{
     return React.createElement(
         "div",
@@ -19,9 +16,7 @@ const App = () =>{
         React.createElement(Chai,{name:"Lemon Tea",cost:"800"}),
         React.createElement(Chai,{name:"Ice Tea",cost:"700"}),
     );
-
 };
-
 const container = document.getElementById("root")
 const root = ReactDOM.createRoot(container)
 root.render(React.createElement(App))
