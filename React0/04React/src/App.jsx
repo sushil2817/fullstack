99c23abe6d20c3675fcea50447eb90dd8e3256a7
sushil2817 <tony281723@gmail.com>
2026-03-27 06:58:04 +0530

    Create App.jsx

--- React0/04React/src/App.jsx
+++ React0/04React/src/App.jsx
@@ -1,7 +1,9 @@
 import {useState,useEffect} from "react"
 import AllUser from "./AllUSer.jsx";
+import { useSpecialChai } from "./hooks/useSpecialchai.js";
 export function App(){
-    
+    const {chai,loading,error} = useSpecialChai();
+    console.log("chai",chai?.data?.data[0].description)
     const [message, setMessge] = useState("Loading...")
     useEffect(()=>{
         fetch(`https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10`)
@@ -18,6 +20,7 @@ export function App(){
             <p>Serving hot chai with react</p>
             <h2>{message}</h2>
             {/* <AllUser/> */}
+            <h3>{chai.data?.data[0].description}</h3>
         </div>
     )
 }
