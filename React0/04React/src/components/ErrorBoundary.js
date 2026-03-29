dd67eaab4deb333377962cfd4d71f48107ea8f1a
sushil2817 <tony281723@gmail.com>
2026-03-29 22:59:03 +0530

    Create class based component.jsx

---
+++ React0/04React/src/components/ErrorBoundary.js
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+export class ErrorBoundary extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            hasError:false,
+            message:""
+        }
+    }
+    componentDidCatch(error,info){
+        console.error("Caught by EB",error,info)
+    }
+    render(){
+        if(this.state.hasError){
+            <div>
+                <h2>Somethign is wrong here</h2>
+                {this.state.message}
+            </div>
+        }
+        return this.props.children;
+    }
+}
