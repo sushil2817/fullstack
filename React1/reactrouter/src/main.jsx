d8d9c2924225ff26e4e4e8f27b351f56f3dc0619
sushil2817 <tony281723@gmail.com>
2026-04-06 22:20:39 +0530

    Learn about Tankstack React Router

---
+++ React1/reactrouter/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import ReactDom from 'react-dom/client'
+import { RouterProvider,createRoute } from '@tanstack/react-router'
+
+import {routeTree} from "./routeTree.gen"
+
+const rootElement  = (document.getElementById('root'))
+if(!rootElement?.innerHTML){
+  const root = ReactDom.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router}></RouterProvider>
+    </StrictMode>
+  )
+}
