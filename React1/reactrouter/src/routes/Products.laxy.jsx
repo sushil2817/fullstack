5f9f71e99e6a0c50914016359812eaafe768c08a
sushil2817 <tony281723@gmail.com>
2026-04-10 21:08:22 +0530

    Changes in react file

--- React1/reactrouter/src/routes/Products.laxy.jsx
+++ React1/reactrouter/src/routes/Products.laxy.jsx
@@ -4,16 +4,17 @@ export const Route = createFileRoute('/Products/laxy')({
   component: RouteComponent,
 })
 
+
 function RouteComponent() {
       return <>
-      <div>
-                Hello "/Products/laxy"!
-                <Link to="/Product/1">Go To Product 1</Link>
-                </br>
-                <Link to="/Product/2">Go To Product 2</Link>
-                <br>
-                <Link to="/Product/3">Go To Product 3</Link>
-                <br/>
+                <div>
+                      Hello "/Products/laxy"!
+                      <Link to="/Product/1">Go To Product 1</Link>
+                      </br>
+                      <Link to="/Product/2">Go To Product 2</Link>
+                      <br>
+                      <Link to="/Product/3">Go To Product 3</Link>
+                      <br/>
                 </div>
       </>
 }
