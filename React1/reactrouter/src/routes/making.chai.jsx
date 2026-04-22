0ea1d6d54b2a737abd6ae5d06b16cb1de18f6333
sushil2817 <tony281723@gmail.com>
2026-04-22 09:16:14 +0530

    style: format making.chai.jsx with consistent spacing

--- React1/reactrouter/src/routes/making.chai.jsx
+++ React1/reactrouter/src/routes/making.chai.jsx
@@ -1,9 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/making/chai')({
+
   component: RouteComponent,
+
 })
 
 function RouteComponent() {
+
   return <div>Hello "/making/chai"!</div>
+
 }
