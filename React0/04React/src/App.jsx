580d26b149e61cc74d41b4c6623557388721b936
sushil2817 <tony281723@gmail.com>
2026-04-24 13:19:38 +0530

    style: clean up whitespace in App.jsx

--- React0/04React/src/App.jsx
+++ React0/04React/src/App.jsx
@@ -6,16 +6,12 @@ export function App() {
     console.log("chai", chai?.data?.data[0].description)
     const [message, setMessge] = useState("Loading...")
     useEffect(() => {
-
         fetch(`https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10`)
-
             .then(res => res.json())
             .then(data => setMessge(data.data.data[0].dob.date))
             .catch(err => setMessge("Failed to Load"))
     }, []);
-
     // setMessge( prev => prev+1)
-
     return (
         <div>
             <h1>Welcom to chai code</h1>
