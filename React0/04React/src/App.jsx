df5d8f51e5a952fda119878902ace464f988467a
sushil2817 <tony281723@gmail.com>
2026-04-25 11:18:47 +0530

    style: format App.jsx with consistent spacing and indentation

--- React0/04React/src/App.jsx
+++ React0/04React/src/App.jsx
@@ -1,16 +1,18 @@
-import {useState,useEffect} from "react"
+import { useState, useEffect } from "react"
 import AllUser from "./AllUSer.jsx";
 import { useSpecialChai } from "./hooks/useSpecialchai.js";
-export function App(){
-    const {chai,loading,error} = useSpecialChai();
-    console.log("chai",chai?.data?.data[0].description)
+export function App() {
+    const { chai, loading, error } = useSpecialChai();
+    console.log("chai", chai?.data?.data[0].description)
     const [message, setMessge] = useState("Loading...")
-    useEffect(()=>{
+    useEffect(() => {
+
         fetch(`https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10`)
-            .then(res=>res.json())
-            .then(data=>setMessge(data.data.data[0].dob.date))
-            .catch(err=>setMessge ("Failed to Load"))
-    },[]);
+
+            .then(res => res.json())
+            .then(data => setMessge(data.data.data[0].dob.date))
+            .catch(err => setMessge("Failed to Load"))
+    }, []);
 
     // setMessge( prev => prev+1)
 
