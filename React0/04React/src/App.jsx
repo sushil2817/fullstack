aa4a7303dfe090f9dae40643cb92770a463bde2e
sushil2817 <tony281723@gmail.com>
2026-04-29 06:25:42 +0530

    style: clean up whitespace and formatting in App.jsx

--- React0/04React/src/App.jsx
+++ React0/04React/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState, useEffect } from "react"
+
 import AllUser from "./AllUSer.jsx";
+
 import { useSpecialChai } from "./hooks/useSpecialchai.js";
+
 export function App() {
+    
     const { chai, loading, error } = useSpecialChai();
+
     console.log("chai", chai?.data?.data[0].description)
+    
     const [message, setMessge] = useState("Loading...")
+    
     useEffect(() => {
         fetch(`https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10`)
             .then(res => res.json())
@@ -12,6 +19,7 @@ export function App() {
             .catch(err => setMessge("Failed to Load"))
     }, []);
     // setMessge( prev => prev+1)
+    
     return (
         <div>
             <h1>Welcom to chai code</h1>
