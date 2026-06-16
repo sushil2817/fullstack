d164483c591e59b2e1f567342d468d1d96f671b9
sushil2817 <tony281723@gmail.com>
2026-06-16 23:11:44 +0530

    Learn about redis

--- ADV. JS/redis/src/index.ts
+++ ADV. JS/redis/src/index.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import express from 'express'
 
 const app = express();
@@ -5,7 +6,13 @@ const PORT = process.env.PORT ?? 8000;
 
 
 app.get("/",(req,res)=>{
-    
+    return res.json({status:'success'});
 })
 
+
+app.get('/books/total',async (req,res)=>{
+    const response =  await axios.get('https://api.freeapi.app/api/v1/public/books')
+    const total = response?.data?.data?.data?.reduce((acc,curr) => curr.pageCount+acc,0);
+    return res.json(total);
+})
 app.listen(PORT,()=>console.log(`Server is running at PORT ${PORT}`));
