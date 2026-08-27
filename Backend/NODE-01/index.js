20bc8003da162571229af0cb5725b3956426b917
sushil2817 <tony281723@gmail.com>
2026-08-28 02:21:38 +0530

    db connection

--- Backend/NODE-01/index.js
+++ Backend/NODE-01/index.js
@@ -8,9 +8,6 @@ fs.writeFile('./test.txt',"Hello World!",()=>{});
 console.log(math());
 console.log(math.add(5,7));
 console.log(math.subtract(5,7));
-
-
-
 function __require(id){
     // ... agar id ./ se shuru hua toh user ki dir me code ko dhundo
     // .. Khud ke internal module mei dhundo
@@ -18,13 +15,8 @@ function __require(id){
     // .. nahi to node_modules mei dhundo
     // .. user ko error throe karo
 }
-
-
 // executeWrapperDxn(exports, require,module, __dirname,__filename, ){
-
 // }
-
-
 // require('fs') = Node built in
 // require('./math') = Current dir mei math name ki file ko  
 // require('../math') = Ek dir bhar jao aur parent mei math file ko dekho
--- Backend/backendProject-1/src/db/index.js
+++ Backend/backendProject-1/src/db/index.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 const connectDB = async () =>{
     try{
         await mongoose.connect(process.env.MONOGO_URI)
-        console.log("MOongoDB Connected");
+        console.log("MoongoDB Connected");
     }catch(error){
         console.error("MongoDB connection failed", error);
         process.exit(1);
     }
 }
+console.log("MongoDB Connected")
 
 export default connectDB;
